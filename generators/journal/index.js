68fba43436aec26048252a554d33c74f907d0567
unknown <kstewart@KSTEWART141013.huronconsultinggroup.com>
2015-10-10 19:09:18 -0500

    name journal files after the date that they are for and allow for multiple journal series to be generated

--- generators/journal/index.js
+++ generators/journal/index.js
@@ -2,20 +2,32 @@
 var yeoman = require('yeoman-generator');
 
 module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.argument('name', {
-      required: false,
-      type: String,
-      desc: 'The subgenerator name'
-    });
+    initializing: function () {
+        this.argument('journalName', {
+            required: false,
+            type: String,
+            desc: 'The name of the journal series'
+        });
 
-    this.log('You called the Myjournal subgenerator with the argument ' + this.name + '.');
-  },
+        if (!this.journalName) {
+            this.journalName = "myjournal";
+        }
+        
+        this.log("You creating a journal file for the '" + this.journalName + "' journal series.");
+    },
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('journal.md'),
-      this.destinationPath('journal/journal.md')
-    );
-  }
+    writing: function () {
+        var today = new Date();
+        var year = today.getFullYear();
+        var month = today.getMonth() + 1;
+        var day = today.getDate();
+        var fileName = 'date' + year + '-' + month + '-' + day + '.md';
+        var path = this.journalName + '/';
+
+        if (this.fs.exists(path + fileName)) {
+            this.log('The journal file your are requesting already exists.');
+        } else {
+            this.fs.copy(this.templatePath('journal.md'), this.destinationPath(path + fileName));
+        }
+    }
 });
